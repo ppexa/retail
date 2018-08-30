fb1bca9f11dfed0c474d777b8153057585a69302
Phill Pexa <ppexa@denniskirk.com>
2018-08-30 14:53:01 -0500

    Forgot to add .idea/ to .gitignore
    Also, followed initial start up for Spring Boot

--- src/main/java/com/demo/myretail/controller/MainController.java
+++ src/main/java/com/demo/myretail/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.demo.myretail.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
 }
