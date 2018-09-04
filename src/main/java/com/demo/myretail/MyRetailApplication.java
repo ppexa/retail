bd4f5925f32063d60d15b5a5d58ba3f225b0a924
ppexa <phillpexa@outlook.com>
2018-09-03 20:15:19 -0500

    Running with db and printing on 8080

--- src/main/java/com/demo/myretail/MyRetailApplication.java
+++ src/main/java/com/demo/myretail/MyRetailApplication.java
@@ -1,33 +1,28 @@
 package com.demo.myretail;
 
-import java.util.Arrays;
-
+import com.demo.myretail.controller.dao.ProductRepository;
+import com.demo.myretail.domain.CurrentPrice;
+import com.demo.myretail.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyRetailApplication {
+public class MyRetailApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRetailApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+	@Override
+	public void run(String... args) throws Exception {
+		productRepository.deleteAll();
+		productRepository.save(new Product("title", new CurrentPrice(12.90, "USD")));
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
+		System.out.println(productRepository.findAll());
 	}
-
 }
--- src/main/java/com/demo/myretail/controller/MainController.java
+++ src/main/java/com/demo/myretail/controller/MainController.java
@@ -1,14 +1,33 @@
 package com.demo.myretail.controller;
 
+import com.demo.myretail.controller.service.ProductService;
+import com.demo.myretail.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@Autowired
+	private ProductService productService;
+
+	@RequestMapping("/products")
+	public @ResponseBody
+	List<Product> products() {
+		return productService.getAllProducts();
+	}
+
+	@RequestMapping("/products/{prodId}")
+	public @ResponseBody
+	Product product(@PathVariable("prodId") String id) {
+		Optional<Product> optionalProduct = productService.getProductById(id);
+		return optionalProduct.isPresent() ? optionalProduct.get(): new Product();
 	}
 
 }
